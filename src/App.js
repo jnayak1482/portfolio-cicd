f2968048f60ced7e8b2e0268cff810d0c7917c51
jnayak1482 <jnayak2220@gmail.com>
2025-01-29 18:46:00 +0530

    this is my first commit:portfolio site

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Jinendra Nayak</h1>
+        <h2>Devops Engineer at Linearloop Private Limited</h2>
+        <p>Welcome to my portfolio! Passionate about Devops and cloud computing.</p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/mr-nayak-024b2a2a8/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Connect with me on LinkedIn
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
